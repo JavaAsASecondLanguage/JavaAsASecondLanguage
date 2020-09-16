4c911d5d7896da734e1a54afc6ef509680ffa5c8
ponomarevandrew <Caf-Lw9-TV5-pku>
2020-09-16 21:40:11 +0300

    palindrome practice

--- lecture01/src/main/java/io/github/javaasasecondlanguage/lecture01/practice2/Palindrome.java
+++ lecture01/src/main/java/io/github/javaasasecondlanguage/lecture01/practice2/Palindrome.java
@@ -11,6 +11,16 @@ public class Palindrome {
      * @throws IllegalArgumentException if str is `null`
      */
     public static boolean isPalindrome(String str) {
-        throw new RuntimeException("Not implemented");
+        if (str == null) {
+            throw new IllegalArgumentException();
+        }
+        int i = 0, j = str.length() - 1;
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
     }
 }
