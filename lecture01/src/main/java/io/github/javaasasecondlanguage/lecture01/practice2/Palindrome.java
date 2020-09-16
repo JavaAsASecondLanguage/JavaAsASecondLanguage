4277559fbe58d4a671ef0f1d252d332f0e7137f8
ponomarevandrew <Caf-Lw9-TV5-pku>
2020-09-16 21:48:50 +0300

    check style fix

--- lecture01/src/main/java/io/github/javaasasecondlanguage/lecture01/practice2/Palindrome.java
+++ lecture01/src/main/java/io/github/javaasasecondlanguage/lecture01/practice2/Palindrome.java
@@ -14,10 +14,12 @@ public class Palindrome {
         if (str == null) {
             throw new IllegalArgumentException();
         }
-        int i = 0, j = str.length() - 1;
+        var i = 0;
+        var j = str.length() - 1;
         while (i < j) {
-            if (str.charAt(i) != str.charAt(j))
+            if (str.charAt(i) != str.charAt(j)) {
                 return false;
+            }
             i++;
             j--;
         }
