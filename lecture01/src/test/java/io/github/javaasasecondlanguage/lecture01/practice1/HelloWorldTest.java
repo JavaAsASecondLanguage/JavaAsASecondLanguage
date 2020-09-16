6dcdc4877b3a1712d8aba5cb38c3c08c8ba46e86
Vladimir Gavrilenko <gavrilenko92@gmail.com>
2020-09-16 20:48:26 +0300

    hello world practice

--- lecture01/src/test/java/io/github/javaasasecondlanguage/lecture01/practice1/HelloWorldTest.java
+++ lecture01/src/test/java/io/github/javaasasecondlanguage/lecture01/practice1/HelloWorldTest.java
@@ -1,14 +1,10 @@
 package io.github.javaasasecondlanguage.lecture01.practice1;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
-@Disabled
 class HelloWorldTest {
-
     @Test
     void getHelloWorld() {
         var hw = new HelloWorld();
