0d06c04ccc5cab9a7e16b429b075e5b1d4441ec0
Kirill Brodt <cyrill.brodt@gmail.com>
2020-09-19 18:59:07 +0000

    Add solution to interval merger

--- homework01/src/main/java/io/github/javaasasecondlanguage/homework01/mergeintervals/IntervalsMerger.java
+++ homework01/src/main/java/io/github/javaasasecondlanguage/homework01/mergeintervals/IntervalsMerger.java
@@ -1,8 +1,14 @@
 package io.github.javaasasecondlanguage.homework01.mergeintervals;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+
+
 public class IntervalsMerger {
     /**
-     * Given array of intervals, merge overlapping intervals and sort them by start in ascending order
+     * Given array of intervals, merge overlapping intervals and sort them by
+     * start in ascending order
      * Interval is defined as [start, end] where start < end
      * <p>
      * Examples:
@@ -16,6 +22,33 @@ public class IntervalsMerger {
      * @throws IllegalArgumentException if intervals is null
      */
     public int[][] merge(int[][] intervals) {
-        throw new RuntimeException("Not implemented");
+        if (null == intervals) {
+            throw new IllegalArgumentException("intervals are null");
+        }
+
+        if (intervals.length <= 1) {
+            return intervals;
+        }
+
+        Arrays.sort(intervals, Comparator.comparingInt(i -> i[0]));
+        int left = intervals[0][0];
+        int right = intervals[0][1];
+        ArrayList<int[]> mergedIntervals = new ArrayList<int[]>();
+        for (int i = 1; i < intervals.length; ++i) {
+            int curLeft = intervals[i][0];
+            int curRight = intervals[i][1];
+            if (curLeft <= right) {
+                right = Math.max(right, curRight);
+                continue;
+            }
+
+            mergedIntervals.add(new int[]{left, right});
+            left = curLeft;
+            right = curRight;
+        }
+
+        mergedIntervals.add(new int[]{left, right});
+
+        return mergedIntervals.toArray(new int[mergedIntervals.size()][]);
     }
 }
