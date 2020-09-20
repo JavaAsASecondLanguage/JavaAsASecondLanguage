9b482aee383dadb44380752c136b1e0ea49ab8da
Sergey Samofalov <samof.sergey@gmail.com>
2020-09-20 15:22:19 +0300

    implement StringCompressor

--- homework01/src/main/java/io/github/javaasasecondlanguage/homework01/compressor/StringCompressor.java
+++ homework01/src/main/java/io/github/javaasasecondlanguage/homework01/compressor/StringCompressor.java
@@ -1,5 +1,7 @@
 package io.github.javaasasecondlanguage.homework01.compressor;
 
+import java.util.Arrays;
+
 public class StringCompressor {
     /**
      * Given an array of characters, compress it using the following algorithm:
@@ -29,6 +31,36 @@ public class StringCompressor {
      * @throws IllegalArgumentException if any char is not in range 'a'..'z'
      */
     public char[] compress(char[] str) {
-        throw new RuntimeException("Not implemented");
+        if (str == null) {
+            throw new IllegalArgumentException("str is null");
+        }
+        if (str.length == 0) {
+            return new char[0];
+        }
+        StringBuilder compressedString = new StringBuilder();
+        char previousCharacter = str[0];
+        int characterCount = 0;
+        for (var character : str) {
+            if (!(character >= 'a' && character <= 'z')) {
+                throw new IllegalArgumentException(String.format(
+                    "char '%c' is not in range 'a'..'z'", character));
+            }
+            if (character != previousCharacter) {
+                populateWithCharacter(compressedString, previousCharacter, characterCount);
+                characterCount = 1;
+                previousCharacter = character;
+            } else {
+                characterCount++;
+            }
+        }
+        populateWithCharacter(compressedString, previousCharacter, characterCount);
+        return compressedString.toString().toCharArray();
+    }
+
+    private void populateWithCharacter(StringBuilder compressedString, char character, int count) {
+        compressedString.append(character);
+        if (count > 1) {
+            compressedString.append(count);
+        }
     }
 }
