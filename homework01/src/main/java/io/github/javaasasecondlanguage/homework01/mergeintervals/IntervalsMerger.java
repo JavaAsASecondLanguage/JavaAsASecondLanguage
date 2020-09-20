248f317e44e02d0943998c17bc6014fadf2f2194
Sergey Samofalov <samof.sergey@gmail.com>
2020-09-20 15:50:44 +0300

    implement IntervalsMerger

--- homework01/src/main/java/io/github/javaasasecondlanguage/homework01/mergeintervals/IntervalsMerger.java
+++ homework01/src/main/java/io/github/javaasasecondlanguage/homework01/mergeintervals/IntervalsMerger.java
@@ -1,8 +1,12 @@
 package io.github.javaasasecondlanguage.homework01.mergeintervals;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class IntervalsMerger {
     /**
-     * Given array of intervals, merge overlapping intervals and sort them by start in ascending order
+     * Given array of intervals, merge overlapping intervals
+     * and sort them by start in ascending order
      * Interval is defined as [start, end] where start < end
      * <p>
      * Examples:
@@ -16,6 +20,30 @@ public class IntervalsMerger {
      * @throws IllegalArgumentException if intervals is null
      */
     public int[][] merge(int[][] intervals) {
-        throw new RuntimeException("Not implemented");
+        if (intervals == null) {
+            throw new IllegalArgumentException("intervals is null");
+        }
+        int[][] copiedIntervals = new int[intervals.length][];
+        if (intervals.length == 0) {
+            return copiedIntervals;
+        }
+        for (int i = 0; i < intervals.length; i++) {
+            copiedIntervals[i] = new int[]{intervals[i][0], intervals[i][1]};
+        }
+        Arrays.sort(copiedIntervals, Comparator.comparing(x -> x[0]));
+        int[][] res = new int[intervals.length][];
+        int currentPos = 0;
+        int currentRightBound = copiedIntervals[currentPos][1];
+        res[currentPos] = new int[]{copiedIntervals[currentPos][0], copiedIntervals[currentPos][1]};
+        for (var interval : copiedIntervals) {
+            if (interval[0] <= currentRightBound) {
+                currentRightBound = Math.max(currentRightBound, interval[1]);
+                res[currentPos][1] = currentRightBound;
+            } else {
+                currentRightBound = interval[1];
+                res[++currentPos] = new int[]{interval[0], interval[1]};
+            }
+        }
+        return Arrays.copyOf(res, currentPos + 1);
     }
 }
