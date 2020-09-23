f203b97c641f81f83851d86245daa9001c82bf57
Sergey Shubin <shubinsa1@gmail.com>
2020-09-23 17:08:58 +0300

    All checks have been passed

--- homework01/src/main/java/io/github/javaasasecondlanguage/homework01/mergeintervals/IntervalsMerger.java
+++ homework01/src/main/java/io/github/javaasasecondlanguage/homework01/mergeintervals/IntervalsMerger.java
@@ -1,9 +1,11 @@
 package io.github.javaasasecondlanguage.homework01.mergeintervals;
 
+import java.util.*;
+
 public class IntervalsMerger {
     /**
-     * Given array of intervals, merge overlapping intervals and sort them by start in ascending order
-     * Interval is defined as [start, end] where start < end
+     * Given array of intervals, merge overlapping intervals and sort them by start in ascending 
+     * order. Interval is defined as [start, end] where start < end
      * <p>
      * Examples:
      * [[1,3][2,4][5,6]] -> [[1,4][5,6]]
@@ -16,6 +18,26 @@ public class IntervalsMerger {
      * @throws IllegalArgumentException if intervals is null
      */
     public int[][] merge(int[][] intervals) {
-        throw new RuntimeException("Not implemented");
+        if (intervals == null) {
+            throw new IllegalArgumentException();
+        }
+        Collections.sort(Arrays.asList(intervals), new Comparator<int[]>() {
+            @Override
+            public int compare(int[] a, int[] b) {
+                return Integer.compare(a[0], b[0]);
+            }
+        });
+
+        LinkedList<int[]> merged = new LinkedList<>();
+        for (int[] interval : intervals) {
+
+            if (merged.isEmpty() || merged.getLast()[1] < interval[0]) {
+                merged.add(interval);
+            } else {
+                merged.getLast()[1] = Math.max(merged.getLast()[1], interval[1]);
+            }
+        }
+
+        return merged.toArray(new int[merged.size()][]);
     }
 }
