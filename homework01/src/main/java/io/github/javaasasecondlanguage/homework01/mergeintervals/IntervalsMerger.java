9c7ebb35fbf7316576445882cb83e205867c5cfe
cheninkiy <cherninkiy@mail.ru>
2020-09-23 20:37:31 +0500

    rebased and fixed new style rules

--- homework01/src/main/java/io/github/javaasasecondlanguage/homework01/mergeintervals/IntervalsMerger.java
+++ homework01/src/main/java/io/github/javaasasecondlanguage/homework01/mergeintervals/IntervalsMerger.java
@@ -4,8 +4,9 @@ import java.util.TreeMap;
 
 public class IntervalsMerger {
     /**
-     * Given array of intervals, merge overlapping intervals and sort them by start in ascending order
-     * Interval is defined as [start, end] where start < end
+     * Given array of intervals, merge overlapping intervals 
+     * and sort them by start in ascending order.
+     * Interval is defined as [start, end] where start < end.
      * <p>
      * Examples:
      * [[1,3][2,4][5,6]] -> [[1,4][5,6]]
