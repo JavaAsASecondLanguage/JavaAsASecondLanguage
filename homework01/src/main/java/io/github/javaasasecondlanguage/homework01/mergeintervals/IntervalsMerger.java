d2756fe358996ae9850a441f4d32e77b06f56814
Yuri Urentsov <yuri.urentsov@gmail.com>
2020-09-23 04:36:52 +0300

    intervals realization

--- homework01/src/main/java/io/github/javaasasecondlanguage/homework01/mergeintervals/IntervalsMerger.java
+++ homework01/src/main/java/io/github/javaasasecondlanguage/homework01/mergeintervals/IntervalsMerger.java
@@ -1,5 +1,13 @@
 package io.github.javaasasecondlanguage.homework01.mergeintervals;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import static java.lang.Math.max;
+
 public class IntervalsMerger {
     /**
      * Given array of intervals, merge overlapping intervals and sort them by start in ascending order
@@ -16,6 +24,37 @@ public class IntervalsMerger {
      * @throws IllegalArgumentException if intervals is null
      */
     public int[][] merge(int[][] intervals) {
-        throw new RuntimeException("Not implemented");
+
+        if (intervals == null) {
+            throw new IllegalArgumentException();
+        }
+
+        if (intervals.length <= 1) {
+            return intervals;
+        }
+
+        List<int[]> inputList = Arrays.asList(intervals);
+
+        // Sort the list of arrays
+        Collections.sort(inputList, new Comparator<int[]>() {
+            public int compare(final int[] o1, final int[] o2) {
+                return Integer.compare(o1[0], o2[0]);
+            }
+        });
+
+        // init result list
+        List<int[]> result = new ArrayList<int[]>();
+        result.add(inputList.get(0));
+
+        for (int i = 1; i < inputList.size(); i++) {
+            if (result.get(result.size() - 1)[1] >= inputList.get(i)[0]) {
+                result.get(result.size() - 1)[1] = max(inputList.get(i)[1], result.get(result.size() - 1)[1]);
+            } else {
+                result.add(inputList.get(i));
+            }
+        }
+
+        int[][] resultInt = new int[0][0];
+        return result.toArray(resultInt);
     }
 }
