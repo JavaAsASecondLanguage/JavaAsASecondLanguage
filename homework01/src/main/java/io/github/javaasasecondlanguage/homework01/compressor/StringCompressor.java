1cca53cf8625d188c6c47a229a23ec4d188846d2
lebedevsergey <diacomltd@mail.ru>
2020-09-23 16:54:25 +0300

    homework01 done

--- homework01/src/main/java/io/github/javaasasecondlanguage/homework01/compressor/StringCompressor.java
+++ homework01/src/main/java/io/github/javaasasecondlanguage/homework01/compressor/StringCompressor.java
@@ -3,16 +3,16 @@ package io.github.javaasasecondlanguage.homework01.compressor;
 public class StringCompressor {
     /**
      * Given an array of characters, compress it using the following algorithm:
-     *
+     * <p>
      * Begin with an empty string s.
      * For each group of consecutive repeating characters in chars:
      * If the group's length is 1, append the character to s.
      * Otherwise, append the character followed by the group's length.
      * Return a compressed string.
-     * </p>
+     * <p>
      * Follow up:
      * Could you solve it using only O(1) extra space?
-     * </p>
+     * <p>
      * Examples:
      * a -> a
      * aa -> a2
@@ -29,6 +29,46 @@ public class StringCompressor {
      * @throws IllegalArgumentException if any char is not in range 'a'..'z'
      */
     public char[] compress(char[] str) {
-        throw new RuntimeException("Not implemented");
+        if (str == null) {
+            throw new IllegalArgumentException("Null argument!");
+        }
+
+        for (var i = 0; i < str.length; i++) {
+            if (str[i] < 'a' || str[i] > 'z') {
+                throw new IllegalArgumentException(String.format("Contains illegal char {0} at position {1}!", i, str[i]));
+            }
+        }
+
+        if (str.length < 2) {
+            return str;
+        }
+
+        var prevChar = str[0];
+        int charCount = 1;
+        String result = "";
+        for (var i = 1; i < str.length; i++) {
+            if (str[i] == prevChar) {
+                charCount++;
+                continue;
+            }
+
+            result = addCompressedChar(result, prevChar, charCount);
+            prevChar = str[i];
+            charCount = 1;
+        }
+
+        result = addCompressedChar(result, prevChar, charCount);
+
+        return result.toCharArray();
+    }
+
+    private String addCompressedChar(String str, char c, int charCount) {
+        str = str + c;
+        if (charCount > 1) {
+            str = str + String.valueOf(charCount);
+        }
+
+        return str;
     }
 }
+
--- homework01/src/main/java/io/github/javaasasecondlanguage/homework01/mergeintervals/IntervalsMerger.java
+++ homework01/src/main/java/io/github/javaasasecondlanguage/homework01/mergeintervals/IntervalsMerger.java
@@ -1,9 +1,25 @@
 package io.github.javaasasecondlanguage.homework01.mergeintervals;
 
+import java.util.*;
+
 public class IntervalsMerger {
+
+    public class IntervalsComparator implements Comparator<int[]> {
+        @Override
+        public int compare(int[] o1, int[] o2) {
+            if (o1[0] < o2[0]) {
+                return -1;
+            } else if (o1[0] > o2[0]) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+    }
+
     /**
-     * Given array of intervals, merge overlapping intervals and sort them by start in ascending order
-     * Interval is defined as [start, end] where start < end
+     * Given array of intervals, merges overlapping intervals and sort by start in ascending order
+     * Interval is defined as [start,end] where start < end
      * <p>
      * Examples:
      * [[1,3][2,4][5,6]] -> [[1,4][5,6]]
@@ -11,11 +27,43 @@ public class IntervalsMerger {
      * [[1,4][2,3]] -> [[1,4]]
      * [[5,6][1,2]] -> [[1,2][5,6]]
      *
-     * @param intervals is a nullable array of pairs [start, end]
+     * @param intervals nullable array of pairs [start, end]
      * @return merged intervals
      * @throws IllegalArgumentException if intervals is null
      */
     public int[][] merge(int[][] intervals) {
-        throw new RuntimeException("Not implemented");
+        if (intervals == null) {
+            throw new IllegalArgumentException("Null argument!");
+        }
+
+        List<int[]> result = new ArrayList<int[]>(Arrays.asList(intervals));
+        Collections.sort(result, new IntervalsComparator());
+
+        int i = 0;
+        while (i < result.size() - 1) {
+            if (!doIntervalsOverlap(result.get(i), result.get(i + 1))) {
+                i += 1;
+                continue;
+            }
+
+            result.set(i, mergeIntervals(result.get(i), result.get(i + 1)));
+            result.remove(i + 1);
+        }
+
+        return result.toArray(new int[][]{});
+    }
+
+    private boolean doIntervalsOverlap(int[] interval1, int[] interval2) {
+        var one = interval1[1] >= interval2[0] && interval1[1] <= interval2[1];
+        var two = interval2[1] >= interval1[0] && interval2[1] <= interval1[1];
+
+        return one || two;
+    }
+
+    private int[] mergeIntervals(int[] interval1, int[] interval2) {
+        return new int[]{
+                Math.min(interval1[0], interval2[0]),
+                Math.max(interval1[1], interval2[1])
+        };
     }
 }
