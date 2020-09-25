9fee63f7eef7c4799c757051037ace3682f1df26
Fedor Butikov <f.butikov@corp.mail.ru>
2020-09-25 11:41:37 +0300

    fix indents

--- homework01/src/main/java/io/github/javaasasecondlanguage/homework01/mergeintervals/IntervalsMerger.java
+++ homework01/src/main/java/io/github/javaasasecondlanguage/homework01/mergeintervals/IntervalsMerger.java
@@ -8,8 +8,8 @@ public class IntervalsMerger {
      * Given array of intervals, merge overlapping intervals and sort by start in ascending order
      * Interval is defined as [start, end] where start < end
      *
-     * <p>Examples: [[1,3][2,4][5,6]] -> [[1,4][5,6]] [[1,2][2,3]] -> [[1,3]] [[1,4][2,3]] -> [[1,4]]
-     * [[5,6][1,2]] -> [[1,2][5,6]]
+     * <p>Examples: [[1,3][2,4][5,6]] -> [[1,4][5,6]] [[1,2][2,3]] -> [[1,3]]
+     * [[1,4][2,3]] -> [[1,4]] [[5,6][1,2]] -> [[1,2][5,6]]
      *
      * @param intervals is a nullable array of pairs [start, end]
      * @return merged intervals
@@ -33,11 +33,11 @@ public class IntervalsMerger {
                 });
         ArrayList<int[]> result = new ArrayList<>();
         result.add(intervals[0]);
-        int current_res_interval = 0;
+        int currentResInterval = 0;
         for (int i = 1; i < intervals.length; ++i) {
-            if (intervals[i][0] <= result.get(current_res_interval)[1]) {
-                if (intervals[i][1] > result.get(current_res_interval)[1]) {
-                    result.get(current_res_interval)[1] = intervals[i][1];
+            if (intervals[i][0] <= result.get(currentResInterval)[1]) {
+                if (intervals[i][1] > result.get(currentResInterval)[1]) {
+                    result.get(currentResInterval)[1] = intervals[i][1];
                 }
             } else {
                 result.add(intervals[i]);
