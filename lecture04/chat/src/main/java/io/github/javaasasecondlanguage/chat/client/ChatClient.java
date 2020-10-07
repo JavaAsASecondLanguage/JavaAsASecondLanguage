c0ef39f427fd94c1b3978dd73e77f5eb5f9c5dfb
ponomarevandrew <Caf-Lw9-TV5-pku>
2020-10-07 20:39:27 +0300

    lecture 4 : practice 1 - Ponomarev Andrei

--- lecture04/chat/src/main/java/io/github/javaasasecondlanguage/chat/client/ChatClient.java
+++ lecture04/chat/src/main/java/io/github/javaasasecondlanguage/chat/client/ChatClient.java
@@ -11,7 +11,7 @@ import java.net.http.HttpResponse;
 public class ChatClient {
     private static final HttpClient client = HttpClient.newHttpClient();
     private static final String PROTOCOL = "http://";
-    private static final String HOST = "localhost";//change to 3.236.217.35 for practice 1
+    private static final String HOST = "3.236.217.35";//change to 3.236.217.35 for practice 1
     private static final String PORT = ":8080";
 
     //POST host:port/chat/login?name=my_name
@@ -39,11 +39,21 @@ public class ChatClient {
     //Body: "msg='my_message'"
     public static HttpResponse<?> say(String name, String msg)
             throws IOException, InterruptedException {
-        throw new RuntimeException("Not implemented");
+        var request = HttpRequest.newBuilder()
+                .POST(HttpRequest.BodyPublishers.ofString(String.format("msg=%s", msg)))
+                .header("Content-Type", "application/x-www-form-urlencoded")
+                .uri(URI.create(PROTOCOL + HOST + PORT + "/chat/say?name=" + name))
+                .build();
+        return client.send(request, HttpResponse.BodyHandlers.ofString());
     }
 
     //GET host:port/chat/online
     public static HttpResponse<?> viewOnline() throws IOException, InterruptedException {
-        throw new RuntimeException("Not implemented");
+        var request = HttpRequest.newBuilder()
+                .GET()
+                .uri(URI.create(PROTOCOL + HOST + PORT + "/chat/online"))
+                .build();
+        var response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        return response;
     }
 }
