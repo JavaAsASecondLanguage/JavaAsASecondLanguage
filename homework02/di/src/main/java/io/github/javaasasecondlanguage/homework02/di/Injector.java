047e5c9eb0591d907b7550edcb2730bc473bc72f
alex <aliaksandr.shcharbitski@gmail.com>
2020-10-27 18:03:13 +0300

    43

--- homework02/di/src/main/java/io/github/javaasasecondlanguage/homework02/di/Injector.java
+++ homework02/di/src/main/java/io/github/javaasasecondlanguage/homework02/di/Injector.java
@@ -2,10 +2,12 @@ package io.github.javaasasecondlanguage.homework02.di;
 
 public class Injector {
     public static <T> T inject(Class<T> clazz) {
+
         throw new RuntimeException("Not implemented");
     }
 
     public static <T> T inject(Class<T> clazz, String qualifier) {
+
         throw new RuntimeException("Not implemented");
     }
 }
