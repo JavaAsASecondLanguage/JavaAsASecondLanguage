b4ebd0af4dcf409cc830905e2e8ac9cb01898a44
rybalkinsd <yan.brikl@gmail.com>
2020-10-28 18:30:02 +0000

    live coding - annotation processor

---
+++ lecture06/annotation-processor-example/app/src/main/java/io/github/javaasasecondlanguage/app/MutableClass.java
@@ -0,0 +1,13 @@
+package io.github.javaasasecondlanguage.app;
+
+import io.github.javaasasecondlanguage.annotation.Immutable;
+
+import java.util.List;
+
+@Immutable
+public class MutableClass {
+    private final List<String> strs = null;
+    public final int anInt = 42;
+    private final String string = "42";
+
+}
---
+++ lecture06/annotation-processor-example/immutable-annotation-processor/build.gradle.kts
@@ -0,0 +1,9 @@
+group = "io.github.javaasasecondlanguage"
+version = "1.0-SNAPSHOT"
+
+
+dependencies {
+    compileOnly("com.google.auto.service:auto-service:1.0-rc6")
+    annotationProcessor("com.google.auto.service:auto-service:1.0-rc6")
+    implementation(project(":immutable-annotation"))
+}
