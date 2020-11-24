1ade9d8d3a0134cc131be21a567318d39c813daf
alex <aliaksandr.shcharbitski@gmail.com>
2020-11-24 15:53:51 +0300

    hw4 (5) utf

--- homework04/build.gradle.kts
+++ homework04/build.gradle.kts
@@ -22,6 +22,7 @@ dependencies {
     implementation("org.graphstream:gs-ui-swing:2.0")
     implementation("org.graphstream:gs-algo:2.0")
     implementation("com.google.code.gson:gson:2.8.6")
+    implementation("org.jetbrains:annotations:19.0.0")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.3.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.3.1")
@@ -49,6 +50,10 @@ dependencies {
 //        jvmArgs("--enable-preview")
 //    }
 //}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
 
 tasks.withType<Test>().configureEach {
     useJUnitPlatform()
